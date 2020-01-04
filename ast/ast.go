c15cb80b42500a89c3dfe3e4b6458414d7adf00a
tshinag <ifeltub889@gmail.com>
2020-01-04 20:40:17 +0900

    Add String method to AST tree Node for debug

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,15 @@
 package ast
 
-import "github.com/tshinag/monkey/token"
+import (
+	"bytes"
+
+	"github.com/tshinag/monkey/token"
+)
 
 // Node is a node of AST tree
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement is the expression of statement
@@ -32,6 +37,16 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // LetStatement implements let statement
 type LetStatement struct {
 	Token token.Token // token.LET トークン
@@ -44,6 +59,22 @@ func (ls *LetStatement) statementNode() {}
 // TokenLiteral implements Node interface
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
 }
 
 // Identifier implements identifier
@@ -59,6 +90,10 @@ func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
 
+func (i *Identifier) String() string {
+	return i.Value
+}
+
 // ReturnStatement implements return statement
 type ReturnStatement struct {
 	Token       token.Token // 'return' トークン
@@ -70,4 +105,37 @@ func (rs *ReturnStatement) statementNode() {}
 // TokenLiteral implements Node interface
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+// ExpressionStatement implements expression statement
+type ExpressionStatement struct {
+	Token      token.Token // 式の最初のトークン
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+// TokenLiteral implements Node interface
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
