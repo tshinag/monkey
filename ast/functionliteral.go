aa88c1cfd17d0b277aea71429c5fe3e1ee6bdcaf
tshinag <ifeltub889@gmail.com>
2020-01-12 18:11:16 +0900

    Add a parser implementation
    - function literal

---
+++ ast/functionliteral.go
@@ -0,0 +1,39 @@
+package ast
+
+import (
+	"bytes"
+	"strings"
+
+	"github.com/tshinag/monkey/token"
+)
+
+// FunctionLiteral implements function literal
+type FunctionLiteral struct {
+	Token      token.Token // 'fn' トークン
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+
+// TokenLiteral implements Node interface
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
