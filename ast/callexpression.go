5b2e6e48268780d3f9da76bb3c61be50bf0e9c25
tshinag <ifeltub889@gmail.com>
2020-01-12 18:13:08 +0900

    Add a parser implementation
    - call expression

---
+++ ast/callexpression.go
@@ -0,0 +1,38 @@
+package ast
+
+import (
+	"bytes"
+	"strings"
+
+	"github.com/tshinag/monkey/token"
+)
+
+// CallExpression implements call expression
+type CallExpression struct {
+	Token     token.Token // The '(' token
+	Function  Expression  // Identifier or FunctionLiteral
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode() {}
+
+// TokenLiteral implements Node interface
+func (ce *CallExpression) TokenLiteral() string {
+	return ce.Token.Literal
+}
+
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
