6bfcc44041a8be4161cce99b8b063afcf282259c
tshinag <ifeltub889@gmail.com>
2020-01-12 10:38:47 +0900

    Add a parser implementation
    - if expression
    - block statement

---
+++ ast/blockstatement.go
@@ -0,0 +1,30 @@
+package ast
+
+import (
+	"bytes"
+
+	"github.com/tshinag/monkey/token"
+)
+
+// BlockStatement implements block statement
+type BlockStatement struct {
+	Token      token.Token // { トークン
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode() {}
+
+// TokenLiteral implements Node interface
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
