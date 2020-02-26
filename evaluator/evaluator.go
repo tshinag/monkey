7d978cfc3ebe2d3093e7c8ee6931a9dd1c485f81
tshinag <ifeltub889@gmail.com>
2020-02-27 00:43:33 +0900

    Add an evaluator implementation
    - infix operator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -25,14 +25,15 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	// expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
-		if node.Value {
-			return TRUE
-		}
-		return FALSE
+		return referenceBooleanObject(node.Value)
 	}
 	return nil
 }
@@ -76,4 +77,63 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 	default:
 		return NULL
 	}
+}
+
+func evalInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if li, ok := left.(*object.Integer); ok {
+		if ri, ok := right.(*object.Integer); ok {
+			return evalIntegerInfixExpression(operator, li, ri)
+		}
+	}
+	return evalObjectInfixExpression(operator, left, right)
+}
+
+func evalObjectInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	switch operator {
+	case "==":
+		return referenceBooleanObject(left == right)
+	case "!=":
+		return referenceBooleanObject(left != right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(
+	operator string,
+	left, right *object.Integer,
+) object.Object {
+	switch operator {
+	case "+":
+		return &object.Integer{Value: left.Value + right.Value}
+	case "-":
+		return &object.Integer{Value: left.Value - right.Value}
+	case "*":
+		return &object.Integer{Value: left.Value * right.Value}
+	case "/":
+		return &object.Integer{Value: left.Value / right.Value}
+	case "<":
+		return referenceBooleanObject(left.Value < right.Value)
+	case ">":
+		return referenceBooleanObject(left.Value > right.Value)
+	case "==":
+		return referenceBooleanObject(left.Value == right.Value)
+	case "!=":
+		return referenceBooleanObject(left.Value != right.Value)
+	default:
+		return NULL
+	}
+}
+
+func referenceBooleanObject(value bool) *object.Boolean {
+	if value {
+		return TRUE
+	}
+	return FALSE
 }
