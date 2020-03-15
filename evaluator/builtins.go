83581f9486ec0c714c67809b7002b497725909b3
tshinag <ifeltub889@gmail.com>
2020-03-15 16:03:24 +0900

    Add built-in function "last"

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -5,6 +5,7 @@ import "github.com/tshinag/monkey/object"
 var builtins = map[string]*object.Builtin{
 	"len":   &object.Builtin{Fn: fnLen},
 	"first": &object.Builtin{Fn: fnFirst},
+	"last":  &object.Builtin{Fn: fnLast},
 }
 
 func fnLen(args ...object.Object) object.Object {
@@ -29,6 +30,22 @@ func fnFirst(args ...object.Object) object.Object {
 	case *object.Array:
 		if len(arg.Elements) > 0 {
 			return arg.Elements[0]
+		}
+		return NULL
+	default:
+		return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
+	}
+}
+
+func fnLast(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments. got=%d, want=1", len(args))
+	}
+	switch arg := args[0].(type) {
+	case *object.Array:
+		length := len(arg.Elements)
+		if length > 0 {
+			return arg.Elements[length-1]
 		}
 		return NULL
 	default:
