6872d23a02326a5492bd2c9ba802303738ab86ba
tshinag <ifeltub889@gmail.com>
2020-03-15 16:00:09 +0900

    Add built-in function "first"

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -3,7 +3,8 @@ package evaluator
 import "github.com/tshinag/monkey/object"
 
 var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{Fn: fnLen},
+	"len":   &object.Builtin{Fn: fnLen},
+	"first": &object.Builtin{Fn: fnFirst},
 }
 
 func fnLen(args ...object.Object) object.Object {
@@ -17,5 +18,20 @@ func fnLen(args ...object.Object) object.Object {
 		return &object.Integer{Value: int64(len(arg.Elements))}
 	default:
 		return newError("argument to `len` not supported, got %s", args[0].Type())
+	}
+}
+
+func fnFirst(args ...object.Object) object.Object {
+	if len(args) != 1 {
+		return newError("wrong number of arguments. got=%d, want=1", len(args))
+	}
+	switch arg := args[0].(type) {
+	case *object.Array:
+		if len(arg.Elements) > 0 {
+			return arg.Elements[0]
+		}
+		return NULL
+	default:
+		return newError("argument to `first` must be ARRAY, got %s", args[0].Type())
 	}
 }
