dafee00910c035ef23000556f7b82f24b797aea0
tshinag <ifeltub889@gmail.com>
2020-03-15 15:54:51 +0900

    Add built-in function "len" for array

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -13,6 +13,8 @@ func lenImpl(args ...object.Object) object.Object {
 	switch arg := args[0].(type) {
 	case *object.String:
 		return &object.Integer{Value: int64(len(arg.Value))}
+	case *object.Array:
+		return &object.Integer{Value: int64(len(arg.Elements))}
 	default:
 		return newError("argument to `len` not supported, got %s", args[0].Type())
 	}
